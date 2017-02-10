3cd4b0743f2555963f824ac091fef91b80461fe1
Rick <panruru1224@hotmail.com>
2017-02-10 14:03:08 +0800

    Fixed currBoard bug Now checks if record already exists

--- record.py
+++ record.py
@@ -1,4 +1,6 @@
 import numpy as np
+import os.path
+
 
 class Record:
     def __init__(self):
@@ -7,15 +9,19 @@ class Record:
 	self.allGameBoards=[]
         self.currGameBoards=[]
     def recordBoard(self,board):
-	self.currGameBoards.append(board)
+        self.currGameBoards.append(np.array(board))
     def recordGame(self,score):
 	self.nGames+=1
-	for board in self.currGameBoards:
-	    self.allGameBoards.append(board)
-	    self.scores.append(score)	
-	self.currGameBoards=[]
+        print score
+        for board in self.currGameBoards:
+            self.allGameBoards.append(board)
+            self.scores.append(score)	
+        self.currGameBoards=[]
     def saveRecord(self):
-	npBoards = np.array(self.allGameBoards).reshape((len(self.allGameBoards),1,22,10))
-	npScores = np.array(self.scores)
-	np.save('data/boards',npBoards)
-	np.save('data/scores',npScores)
+        npBoards = np.array(self.allGameBoards).reshape((len(self.allGameBoards),1,22,10))
+        npScores = np.array(self.scores)
+        n=0
+        while os.path.isfile('data/boards%d.npy' % n):
+            n+=1
+        np.save('data/boards%d.npy' % n,npBoards)
+        np.save('data/scores%d.npy' % n,npScores)
