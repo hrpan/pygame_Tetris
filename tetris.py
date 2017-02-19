ce624b0d0808da2ab188fe75bc69a080ffe1bf13
Rick <panruru1224@hotmail.com>
2017-02-19 19:27:35 +0800

    Now prints stats at the end of game

--- tetris.py
+++ tetris.py
@@ -4,17 +4,18 @@ Created on Tue Feb 07 03:57:47 2017
 
 @author: Rick
 """
-
 import sys, pygame
 import vis
 import core
 import predict
 import record
+
 mode='AI'
 #mode='PLAY'
 
 game = core.Tetris()
 rec = record.Record()
+
 def null():
 	return
 
@@ -29,12 +30,18 @@ game.start()
 
 tick=-1
 
+aiGames=500
+
 while 1:
     if game.over:
 	if mode=='AI':
-	    rec.recordGame(game.score)
-	    if rec.nGames==10:
-		break
+	    rec.recordGame()
+            avgS=rec.averageScore()
+            sys.stdout.write('\rGame: %4d/%d   R-Score(avg): %.4f   H-Score(avg): %.4f' % (rec.nGames,aiGames,avgS[1],avgS[0]))
+            sys.stdout.flush()
+            if rec.nGames==aiGames:
+                print ''
+                break
 	    game.reset()
 	    game.start()
 	else:
@@ -44,6 +51,7 @@ while 1:
         
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
+            print ''
             sys.exit()
         if event.type==pygame.KEYDOWN:
             if event.key==pygame.K_LEFT:
@@ -58,33 +66,42 @@ while 1:
                 game.rotateCW()
             if event.key==pygame.K_SPACE:
                 game.moveDrop()
-                
+    """ 
     keys=pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         game.moveLeft()
+        pygame.time.delay(50)
     elif keys[pygame.K_RIGHT]:
         game.moveRight()
+        pygame.time.delay(50)
     if keys[pygame.K_DOWN]:
         game.moveDown()
-        
+        pygame.time.delay(50)
+    """
     
     
     if mode=='AI':
-	for i in range(4):
+	for i in range(3):
             boards=[game.previewBoard(i) for i in range(5)]
 	    bestOper=predict.predictOper(boards)
-	    opers[bestOper]()
-        rec.recordBoard(game.getState())
+            if bestOper==4:
+                break
+            opers[bestOper]()
+            rec.recordBoard(game.getState(),game.score)
 	game.nextIter()
+
     elif mode=='PLAY':
-        pygame.time.delay(50)
+        pygame.time.delay(10)
         if (pygame.time.get_ticks()-tick)>1500:
             tick = pygame.time.get_ticks()
             game.nextIter()
 
+    game.clearLines()
+"""
     vis.drawScore(game,screen,font)
     vis.drawBoard(game,screen)
-        
+    
     pygame.display.flip()
+"""
 if mode=='AI':
     rec.saveRecord()
