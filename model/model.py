1d07534fc1d213ba344839f744d805e64b382d50
Rick <panruru1224@hotmail.com>
2017-02-19 19:24:39 +0800

    Testing new model

--- model/model.py
+++ model/model.py
@@ -2,21 +2,23 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation, Convolution2D, Flatten, MaxPooling2D, Dropout
 
 model = Sequential()
-model.add(Convolution2D(24,6,6,input_shape=(1,22,10)))
+model.add(Convolution2D(32,4,4,subsample=(2,2),input_shape=(1,22,10)))
 model.add(Activation('relu'))
-model.add(Convolution2D(12,3,3))
+model.add(Convolution2D(40,3,3))
 model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Convolution2D(48,4,1))
+model.add(Activation('relu'))
+#model.add(MaxPooling2D(pool_size=(2,2)))
 model.add(Flatten())
-model.add(Dense(9))
-model.add(Activation('relu'))
-model.add(Dense(9))
+model.add(Dense(250))
 model.add(Activation('relu'))
 model.add(Dense(1))
-model.add(Activation('sigmoid'))
-model.compile(optimizer='sgd',loss='binary_crossentropy')
+#model.add(Activation('sigmoid'))
+#model.compile(optimizer='sgd',loss='binary_crossentropy')
 
-#model.add(Activation('linear'))
-#model.compile(optimizer='sgd',loss='mse')
+model.add(Activation('linear'))
+model.compile(optimizer='rmsprop',loss='mse')
+
+print model.summary()
 
 model.save('model.h5')
