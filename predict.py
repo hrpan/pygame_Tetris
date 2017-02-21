24ac326ef0a8afccec6ae829a283c234dcaa6f84
hrpan <b00502006@ntu.edu.tw>
2017-02-21 22:18:37 +0800

    add slope for sigmoid parameter

--- predict.py
+++ predict.py
@@ -6,13 +6,15 @@ model = load_model('model/model.h5')
 
 opers=np.arange(5)
 
+slope=0.2
+
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
 def predictOper(boards,cycle):
     boards = np.array(boards).reshape((5,1,22,10))
     predicts = model.predict(boards)
-    epsilon=1-sigmoid(cycle) 
+    epsilon=1-sigmoid(slope*cycle) 
     prob=np.ones((5))*(epsilon/4)
     prob[np.argmax(predicts)]=1-epsilon
     roll=np.random.choice(opers,p=prob)
