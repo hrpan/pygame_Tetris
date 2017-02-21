a2ecb0cde18766921d7688ade8ff99de298e8837
Rick <panruru1224@hotmail.com>
2017-02-21 19:39:30 +0800

    Add epsilon decay (using sigmoid)

--- predict.py
+++ predict.py
@@ -1,20 +1,18 @@
 from keras.models import load_model
-import theano
 import numpy as np
 
-#theano.config.openmp=False
 
 model = load_model('model/model.h5')
 
 opers=np.arange(5)
 
-epsilon=0.1
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
 
-def predictOper(boards):
+def predictOper(boards,cycle):
     boards = np.array(boards).reshape((5,1,22,10))
     predicts = model.predict(boards)
-    #print predicts.shape, predicts
-    
+    epsilon=1-sigmoid(cycle) 
     prob=np.ones((5))*(epsilon/4)
     prob[np.argmax(predicts)]=1-epsilon
     roll=np.random.choice(opers,p=prob)
