8070ebb91bb555b0c1b96bbda5f96f3e6f31b3d4
hrpan <b00502006@ntu.edu.tw>
2017-02-22 18:38:25 +0800

    fix indentation, ncycle is now class member

--- record.py
+++ record.py
@@ -16,41 +16,50 @@ def evalBoard(board,score):
     return score-0.2*penalty
 
 class Record:
-    def __init__(self,cycle):
+    def __init__(self,cycle,ncycle):
         self.cycle=cycle
-	self.nGames=0
-	self.allScores=[]
+        self.ncycle=ncycle
+        self.nGames=0
+        self.allScores=[]
         self.rawScores=[0]
         self.currScores=[]
-	self.allGameBoards=[]
+        self.allGameBoards=[]
         self.currGameBoards=[]
+
     def recordBoard(self,board,score):
         self.rawScores[-1]=score
         self.currScores.append(evalBoard(np.array(board),score))
         self.currGameBoards.append(np.array(board))
+
     def recordGame(self):
-	self.nGames+=1
+        self.nGames+=1
         self.rawScores.append(0)
         length=len(self.currGameBoards)
+
         for i,board in enumerate(self.currGameBoards):
             self.allGameBoards.append(board)
+
             if i==length-1:
                 reward=-5
             else:
                 reward=self.currScores[i+1]-self.currScores[i]
+
             self.allScores.append(reward)
             if reward!=0:
                 for j in range(1,decay_length):
                     self.allScores[-1-j]+=reward*np.power(gamma,j)
+
         self.currGameBoards=[]
         self.currScores=[]
     def scoreStats(self):
         return np.average(self.rawScores),np.var(self.rawScores),np.average(self.allScores)
+
     def saveRecord(self):
         npBoards = np.array(self.allGameBoards).reshape((len(self.allGameBoards),1,22,10))
         npScores = np.array(self.allScores)
+        del self.rawScores[-1]
+        nprScores = np.array(self.rawScores)
         n=self.cycle%ncycle
-        #while os.path.isfile('data/boards%d.npy' % n):
-        #    n+=1
         np.save('data/boards%d.npy' % n,npBoards)
         np.save('data/scores%d.npy' % n,npScores)
+        np.save('data/rScores%d.npy' % n,nprScores)
