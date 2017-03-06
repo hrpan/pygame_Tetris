d4805922ad3f5ffe2c55c5abdc3b40b8ec57cd10
hrpan <b00502006@ntu.edu.tw>
2017-03-07 04:30:42 +0800

    Now loads model according to config

--- predict.py
+++ predict.py
@@ -7,10 +7,16 @@ slope=0.01
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
+def expected_value(x):
+    tmp=0
+    for i,prob in enumerate(x):
+        tmp+=i*prob
+    return tmp
+
 class Predict:
     def __init__(self,cycle):
         cfg=Config()
-        self.model=load_model('model/model.h5')
+        self.model=load_model(cfg.modelFile)
         self.cycle=cycle
         self.ncycle=cfg.ncycle
         self.eps = max(np.exp(-cfg.eps_decay_rate*cycle),cfg.eps_pred)
@@ -19,7 +25,8 @@ class Predict:
         boards = np.array(boards).reshape((5,22,10,1))
         if np.random.rand()>self.eps:
             predicts = self.model.predict(boards)
-            roll = np.argmax(predicts)
+            expected = np.array([expected_value(x) for x in predicts])
+            roll = np.argmax(np.array(expected))
         else:
             roll = np.random.choice(range(5))
         return roll
