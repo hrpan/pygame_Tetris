3ef4f5416ad3002ad635b750e2e15df727bed685
hrpan <b00502006@ntu.edu.tw>
2017-03-08 14:32:01 +0800

    Now use exec in config.py

--- config.py
+++ config.py
@@ -3,33 +3,8 @@ class Config:
         f=open('cfg')
         lines=f.read().splitlines()
         for line in lines:
-            if 'NCYCLE=' in line:
-                self.ncycle=int(line.replace('NCYCLE=',''))
-            elif 'BATCH_SIZE=' in line:
-                self.batch_size=int(line.replace('BATCH_SIZE=',''))
-            elif 'EPOCHS=' in line:
-                self.epochs=int(line.replace('EPOCHS=',''))
-            elif 'GAMMA_TD=' in line:
-                self.gamma_td=float(line.replace('GAMMA_TD=',''))
-            elif 'EPS_TD=' in line:
-                self.eps_td=float(line.replace('EPS_TD=',''))
-            elif 'EPS_PRED=' in line:
-                self.eps_pred=float(line.replace('EPS_PRED=',''))
-            elif 'USE_SAMPLE_WEIGHT=' in line:
-                if line.replace('USE_SAMPLE_WEIGHT=','')=='True':
-                    self.use_sample_weight=True
-                else:
-                    self.use_sample_weight=False
-            elif 'AIGAMES=' in line:
-                self.aiGames=int(line.replace('AIGAMES=',''))
-            elif 'GAMEMODE=' in line:
-                self.mode=line.replace('GAMEMODE=','')
-            elif 'EPS_DECAY_RATE=' in line:
-                self.eps_decay_rate=float(line.replace('EPS_DECAY_RATE=',''))            
-            elif 'PRETRAIN=' in line:
-                if line.replace('PRETRAIN=','')=='True':
-                    self.preTrain=True
-                elif line.replace('PRETRAIN=','')=='False':
-                    self.preTrain=False 
-            elif 'MODELFILE=' in line:
-                self.modelFile=line.replace('MODELFILE=','')
+            if '//' in line or line.strip()=='':
+                continue
+            
+            parse = 'self.'+line
+            exec(parse)
