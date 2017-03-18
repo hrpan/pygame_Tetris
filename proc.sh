94322d58740da09de292a22112fe61c3dd09a902
hrpan <b00502006@ntu.edu.tw>
2017-03-18 08:06:10 +0800

    Add support for extract

--- proc.sh
+++ proc.sh
@@ -1,8 +1,10 @@
 #/bin/bash
 cycles=$1
 rm data/*.npy
+rm data/sample/*.npy
 for ((i=0;i<$cycles;i++)); do
-	echo "Cycle: $i"
-	python tetris.py $i
-	python train.py $i
+    echo "Cycle: $i"
+    python tetris.py $i
+    python train.py $i
+    python extract.py $i
 done
