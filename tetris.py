e3feb7464389900d2c05e17dff41861afd662a2d
hrpan <b00502006@ntu.edu.tw>
2017-03-18 08:08:57 +0800

    Adapt to q-learning

--- tetris.py
+++ tetris.py
@@ -79,12 +79,12 @@ while 1:
             tick = pygame.time.get_ticks()
             game.nextIter()
     elif cfg.gamemode=='AI':
-        for i in range(3):
-            boards=[game.previewBoard(i) for i in range(5)]
-            bestOper=pred.predictOper(boards)
+        for i in range(cfg.aisteps):
+            #boards=[game.previewBoard(i) for i in range(5)]
+            bestOper=pred.predictOper(game.getState())
             if bestOper!=4:
                 opers[bestOper]()
-            rec.recordBoard(game.getState(),game.score)
+            rec.recordBoard(game.getState(),game.score,bestOper)
         game.nextIter()
 
 
